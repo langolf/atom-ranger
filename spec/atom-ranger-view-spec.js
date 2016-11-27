3763dae96f55d4329815b660d8be518cda379b50
Aleksandr Langolf <sasha.langolf@gmail.com>
2016-11-27 14:09:09 +0300

    Init

---
+++ spec/atom-ranger-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomRangerView from '../lib/atom-ranger-view';
+
+describe('AtomRangerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
